78c06016a68d329d37723921ccc3a7303f390859
Alex-Jr <50220189+Alex-Jr@users.noreply.github.com>
2025-05-10 07:37:26 -0300

    feat: add perfectionist plugin to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 
 import eslint from '@eslint/js';
+import perfectionist from 'eslint-plugin-perfectionist'
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
+  perfectionist.configs['recommended-natural']
 );
