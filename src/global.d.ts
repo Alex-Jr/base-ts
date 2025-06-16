0f00bd7db1f05cc7abb133bd9a8cfb0e0a8a6319
Alex-Jr <50220189+Alex-Jr@users.noreply.github.com>
2025-06-15 21:36:29 -0300

    feat: add environment configuration and TypeScript global definitions

---
+++ src/global.d.ts
@@ -0,0 +1,10 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      LOG_LEVEL: 'error' | 'info' |  'warn' ;
+      NODE_ENV: 'development' | 'production' | 'test';
+    }
+  }
+}
+
+export {};
