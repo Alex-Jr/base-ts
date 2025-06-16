a23c8f662472ce9030f20904f4a8af13a6643369
Alex-Jr <50220189+Alex-Jr@users.noreply.github.com>
2025-06-15 21:55:36 -0300

    feat: add globalIgnores configuration to eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,11 @@
 import eslint from "@eslint/js";
 import perfectionist from "eslint-plugin-perfectionist";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import { globalIgnores } from "eslint/config";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+  globalIgnores(["out/*"]),
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
